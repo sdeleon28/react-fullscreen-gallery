d764148ad67fe8e60c081cb097c7d382f92bc2bc
Santiago de Leon <sdeleon28@gmail.com>
2017-01-28 13:41:03 -0300

    Set main container as `position: relative` so that child elements with `position: absolute` (such as the title) won't float around other portions of the page when the gallery is not the only element

--- src/js/components/Gallery.js
+++ src/js/components/Gallery.js
@@ -14,6 +14,7 @@ const styles = {
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     overflow: 'hidden',
+    position: 'relative',
   },
   titleContainer: {
     display: 'block',
