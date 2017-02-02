9223d48f68ff0a71322386cea6479ec9e0dddd02
Santiago de Leon <sdeleon28@gmail.com>
2017-02-02 18:24:00 -0300

    Set up react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,39 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+/* eslint-disable */
+var path = require('path');
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const debug = process.env.NODE_ENV !== 'production';
+var debug = process.env.NODE_ENV !== 'production';
 
-
-const PLUGINS = [
+var COPY_PLUGINS = [
   new CopyWebpackPlugin([{ from: './examples/index.html' }]),
   new CopyWebpackPlugin([{ from: './deploy_key.enc' }]),
 ];
 
+var DEBUG_PLUGINS = [
+  new webpack.HotModuleReplacementPlugin(),
+  ...COPY_PLUGINS,
+];
+
+var PRODUCTION_PLUGINS = [
+  ...COPY_PLUGINS,
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+];
+
+var ENTRY_FILE = './examples/src/js/index.js';
+
 module.exports = {
   context: __dirname,
   devtool: debug ? '#eval-source-map' : null,
-  entry: './examples/src/js/index.js',
+  entry: debug ? ['webpack-hot-middleware/client', ENTRY_FILE] : ENTRY_FILE,
   module: {
     loaders: [
       {
         test: /\.js?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-        },
+        loaders: ['react-hot', 'babel-loader'],
       },
       {
         test: /\.css$/,
@@ -42,10 +53,5 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, '/examples/dist'),
   },
-  plugins: debug ? PLUGINS : [
-    ...PLUGINS,
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  plugins: debug ? DEBUG_PLUGINS : PRODUCTION_PLUGINS,
 };
